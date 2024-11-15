24f05e5362b1f129fdb8ea1bf6c35732c022428e
Joseph Eaton <josepheaton04@outlook.com>
2024-11-15 11:48:15 +0000

    Begin implementing CustomStack class.

---
+++ GenericsChallenge2/GenericsChallenge2/Classes/CustomStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsChallenge2.Classes
+{
+    internal class CustomStack<T> : IEnumerable<T>
+    {
+        public T[] Items { get; private set; }
+
+        public CustomStack(T[] items)
+        {
+            Items = items;
+        }
+
+        public void Push(T item)
+        {
+            T[] temp = new T[0];
+            Array.Copy(Items, temp, Items.Length + 1);
+            temp[^1] = item;
+            Items = temp;
+        }
+        public void Pop()
+        {
+            T[] temp = new T[Items.Length - 1];
+
+            for (int i = 0; i < Items.Length - 1; i++)
+            {
+                temp[i] = Items[i];
+            }
+            Items = temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)Items).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+    }
+}
--- GenericsChallenge2/GenericsChallenge2/Program.cs
+++ GenericsChallenge2/GenericsChallenge2/Program.cs
@@ -1,11 +1,65 @@
-﻿namespace GenericsChallenge2
+﻿using GenericsChallenge2.Classes;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GenericsChellenge
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Testing!");
+            /*var testList = new List<string>() { "hello", "world" };
+            var newDict = new Dictionary<int, string>
+            {
+                {1, "peas" },
+                {2, "ye;low" },
+            };
+
+            static T GetLast<T>(T[] array)
+            {
+                return array[^1];
+            }
+            
+            double[] doubleArray = { 10.5, 60.5, 2.2, 8.76, 6.1111 };
+            string[] stringArray = { "hello", "world", "it's", "northcoders!" };
+
+            Console.WriteLine(GetLast(ints));
+            Console.WriteLine(GetLast(doubleArray));
+            Console.WriteLine(GetLast(stringArray));
+
+            Stack<string> favouriteBooks = new Stack<string>();
+            favouriteBooks.Push("The Bible");
+            favouriteBooks.Push("The Quran");
+            favouriteBooks.Push("The Hungry Caterpillar");
+            Console.WriteLine(favouriteBooks.Count);
+            Console.WriteLine(favouriteBooks.Pop());
+            Console.WriteLine(favouriteBooks.Count);
+
+            foreach (string book in favouriteBooks)
+            {
+                Console.WriteLine(book);
+            }*/
+
+            int[] numbers = { 1, 2, 3 };
+            CustomStack<int> stack1 = new CustomStack<int>(numbers);
+            foreach (int i in stack1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            stack1.Pop();
+
+            foreach (int i in stack1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            stack1.Push(6);
+
+
         }
     }
 }
