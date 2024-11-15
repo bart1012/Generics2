cba200d68fb342412a82f80ce0cd776fe585019c
bart <bielski.bart1012@gmail.com>
2024-11-15 13:50:48 +0000

    Add the Mission abstract class

---
+++ GenericsChallenge2/GenericsChallenge2/Classes/Mission.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsChallenge2.Classes
+{
+    abstract class Mission<T>
+    {
+        public string Location { get; private set; }
+        public int Reward { get; private set; }
+        public List<T> HeroesDeployed { get; }
+    }
+}
+
+
