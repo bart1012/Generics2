090076f950a37bbab6c7115e948c61de63b9500a
bart <bielski.bart1012@gmail.com>
2024-11-15 10:37:43 +0000

    Inital commit

---
+++ GenericsChallenge2/GenericsChallenge2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsChallenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
